68d944b6c11033413f6f59431b5b2558da485280
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-08-19 16:18:48 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$CO4C59RaMIe3BpnkJWzLGevY6iRCGWudTthaLV.MFe9Rwo/880iaC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$CO4C59RaMIe3BpnkJWzLGevY6iRCGWudTthaLV.MFe9Rwo/880iaC');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
